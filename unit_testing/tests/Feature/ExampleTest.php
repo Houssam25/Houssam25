94804419878b9586bdc11d134b8b79993f53aa17
houssam <ayoubihouss22@gmial.com>
2024-01-16 13:29:16 +0100

    update

---
+++ unit_testing/tests/Feature/ExampleTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertOk();
+    }
+
+    
+    // testTheApplicationReturnsASuccessfulResponse() just 2 resp ¿
+
+    public function  testTheApplicationReturnsASuccessfulResponse() 
+    {
+        $response = $this->get('/');
+
+        $response->assertOk();
+    }
+}
